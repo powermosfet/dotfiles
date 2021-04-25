47a856d2b01494a5a7ab86d06ad059b34bafc82c
Asmund Berge <herrasbe@gmail.com>
2021-04-25 17:00:25 +0200

    New install on laptop

--- setup.sh
+++ setup.sh
@@ -10,3 +10,41 @@ then
 else
 	exe ln -s $(dirname "$BASH_SOURCE")/vim ~/.vim 
 fi
+
+# Xmonad
+if [[ -f ~/.xmonad/xmonad.hs ]]
+then
+	echo "~/.xmonad/xmonad.hs exists already. skipping."
+
+else
+	exe mkdir -p ~/.xmonad
+	exe ln -s $(dirname "$BASH_SOURCE")/xmonad.hs ~/.xmonad/xmonad.hs
+fi
+
+# Xmobar
+if [[ -f ~/.xmobarrc ]]
+then
+	echo "~/.xmobarrc exists already. skipping."
+
+else
+	exe ln -s $(dirname "$BASH_SOURCE")/xmobarrc ~/.xmobarrc 
+fi
+
+# kitty
+if [[ -d ~/.config/kitty ]]
+then
+	echo "~/.config/kitty exists already. skipping."
+
+else
+	exe mkdir -p ~/.config
+	exe ln -s $(dirname "$BASH_SOURCE")/kitty ~/.config/kitty
+fi
+
+# tmux
+if [[ -f ~/.tmux.conf ]]
+then
+	echo "~/.tmux.conf exists already. skipping."
+
+else
+	exe ln -s $(dirname "$BASH_SOURCE")/tmux.conf ~/.tmux.conf
+fi
--- xmonad.hs
+++ xmonad.hs
@@ -1,41 +1,159 @@
 import XMonad
+import Data.Monoid
+import System.Exit
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
+
 import qualified XMonad.StackSet as W
-import XMonad.Util.EZConfig(additionalKeysP)
-import System.IO
+import qualified Data.Map        as M
 
-myManageHook = manageDocks <+> composeAll
-    [ className =? "Gimp"      --> doFloat
-    , className =? "Vncviewer" --> doFloat
-    , className =? "Pidgin"    --> doFloat
-    , className =? "trayer"    --> doIgnore
+myTerminal           = "kitty"
+myBorderWidth        = 3
+myModMask            = mod4Mask
+myWorkspaces         = ["1:term","2:web","3:keepass","4","5","6","7","8","9"]
+myNormalBorderColor  = "#555555"
+myFocusedBorderColor = "#f9ba25"
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+
+    -- launch a terminal
+    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+    -- launch rofi
+    , ((modm,               xK_p     ), spawn "rofi -show run")
+
+    , ((modm .|. controlMask, xK_q), spawn "slock")
+    -- close focused window
+    , ((modm .|. shiftMask, xK_c     ), kill)
+
+    -- Rotate through the available layout algorithms
+    , ((modm,               xK_space ), sendMessage NextLayout)
+
+    --  Reset the layouts on the current workspace to default
+    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+
+    -- Resize viewed windows to the correct size
+    , ((modm,               xK_n     ), refresh)
+
+    -- Move focus to the next window
+    , ((modm,               xK_Tab   ), windows W.focusDown)
+
+    -- Move focus to the master window
+    , ((modm,               xK_m     ), windows W.focusMaster  )
+
+    -- Swap the focused window and the master window
+    , ((modm,               xK_Return), windows W.swapMaster)
+
+    -- Swap the focused window with the next window
+    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
+
+    -- Swap the focused window with the previous window
+    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
+
+    -- Shrink the master area
+    , ((modm,               xK_h     ), sendMessage Shrink)
+
+    -- Expand the master area
+    , ((modm,               xK_l     ), sendMessage Expand)
+
+    -- Push window back into tiling
+    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
+
+    -- Increment the number of windows in the master area
+    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
+
+    -- Deincrement the number of windows in the master area
+    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
+
+    -- Quit xmonad
+    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+
+    -- Restart xmonad
+    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
     ]
+    ++
 
-myTerminal :: String
-myTerminal = "gnome-terminal"
+    --
+    -- mod-[1..9], Switch to workspace N
+    --
+    -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N
+    --
+    [((m .|. modm, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    ++
 
-myKeys =
-        [ ("<Print>", spawn "scrot")
-        , ("<Pause>", spawn "~/bin/toggleTouchpad")
-        , ("<XF86AudioRaiseVolume>", spawn "amixer set Master 10%+")
-        , ("<XF86AudioLowerVolume>", spawn "amixer set Master 10%-")
-        , ("<XF86AudioMute>", spawn "amixer set Master toggle")
-        , ("M-p", spawn "dmenu_run")
-        , ("M-d", spawn "extdisplaytoggle")
-        ]
-		++
-		[(mask ++ "M-" ++ [key], screenWorkspace scr >>= flip whenJust (windows . action))
-			| (key, scr)  <- zip "wer" [1,0,2] -- was [0..] *** change to match your screen order ***
-			, (action, mask) <- [ (W.view, "") , (W.shift, "S-")]
-		]
+    --
+    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+    --
+    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_r, xK_d, xK_w] [0..]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
-main = do
-    xmonad $ defaultConfig
-        { manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
-                        <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , terminal = myTerminal
-        , modMask = mod4Mask     -- Rebind Mod to the Windows key
-        } `additionalKeysP` myKeys
+
+
+
+myLayout = tiled ||| Mirror tiled ||| Full
+  where
+    -- default tiling algorithm partitions the screen into two panes
+    tiled   = Tall nmaster delta ratio
+
+    -- The default number of windows in the master pane
+    nmaster = 1
+
+    -- Default proportion of screen occupied by master pane
+    ratio   = 1/2
+
+    -- Percent of screen to increment by when resizing panes
+    delta   = 3/100
+
+------------------------------------------------------------------------
+-- Status bars and logging
+
+------------------------------------------------------------------------
+-- Startup hook
+
+-- Perform an arbitrary action each time xmonad starts or is restarted
+-- with mod-q.  Used by, e.g., XMonad.Layout.PerWorkspace to initialize
+-- per-workspace layout choices.
+--
+-- By default, do nothing.
+--
+-- * NOTE: EwmhDesktops users should use the 'ewmh' function from
+-- XMonad.Hooks.EwmhDesktops to modify their defaultConfig as a whole.
+-- It will add initialization of EWMH support to your custom startup
+-- hook by combining it with ewmhDesktopsStartup.
+--
+myStartupHook = return ()
+
+
+-- XMOBAR
+--
+-- Command to launch the bar.
+myBar = "xmobar"
+
+-- Custom PP, configure it as you like. It determines what is being written to the bar.
+myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+
+-- Key binding to toggle the gap for the bar.
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+
+------------------------------------------------------------------------
+-- Now run xmonad with all the defaults we set up.
+
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey defaultConfig {
+      -- simple stuff
+          terminal           = myTerminal
+        , borderWidth        = myBorderWidth
+        , modMask            = myModMask
+        , workspaces         = myWorkspaces
+        , normalBorderColor  = myNormalBorderColor
+        , focusedBorderColor = myFocusedBorderColor
+
+      -- key bindings
+        , keys               = myKeys
+
+      -- hooks, layouts
+        , startupHook        = myStartupHook
+    }
